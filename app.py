2c16302ccad0aca5a8763d5c590baccb32d8a9e0
kintaclausin01 <kawabe.hiroki19@ae.k.u-tokyo.ac.jp>
2021-07-29 15:34:45 +0900

    Web App global change

--- app.py
+++ app.py
@@ -592,6 +592,6 @@ def userSelected():
 
 if __name__ == '__main__':
     #socketio.run(app, host='0.0.0.0', port=5001, debug=True)
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0',  port=80, debug=True)
 
 #http://localhost:3000/
